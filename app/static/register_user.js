984b24b1beeb21375c0d941e31418e7d0569f8bf
HolbieWan <8841@holbertonstudents.com>
2024-11-21 18:07:30 +0100

    Update: register_new_user.html , Add: register_user.js to send post request with user payload from the user-form - Everything works fine, can know add new user if looged in with admin rights

---
+++ app/static/register_user.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const register_form = document.getElementById('register-user-form');
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    }
+
+    register_form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+
+        const token = getCookie('jwt_token');
+        console.log('Retrieved Token:', token); // Debugging log
+
+        if (!token) {
+            console.error('Token is missing');
+            window.location.href = '/login';
+            return;
+        }
+
+        const first_name = document.getElementById("user_first_name").value;
+        const last_name = document.getElementById("user_last_name").value;
+        const email = document.getElementById("user_email").value;
+        const password = document.getElementById("user_password").value;
+
+        const userData = {
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password,
+        };
+
+        try {
+            const response = await fetch('/api/v1/users/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(userData)
+            });
+
+            console.log('Register user response status:', response.status); // Debugging log
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error response:', errorData);
+                alert(`Error: ${errorData.msg}`);
+                return;
+            }
+
+            const user_data = await response.json();
+            console.log('User registered successfully:', user_data);
+
+            // Redirect to the login page
+            window.location.href = '/HBnB/login';
+
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    });
+});
