a2fab526fa6a200781aa4266ea0ac106cd7bda72
HolbieWan <8841@holbertonstudents.com>
2024-11-21 12:22:21 +0100

    Add: new route register in routes_FrontEnd.py, new register_new_user.html with form to register new user, Update: header to handle changes in links for active page; update css form register- user-form styling; Renamed place.html to place_details.html for clearer description

--- app/api/v1/routes_FrontEnd.py
+++ app/api/v1/routes_FrontEnd.py
@@ -9,16 +9,20 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_page='places')
 
 @home_bp.route('/place')
 def home_place():
-    return render_template('place.html')
+    return render_template('place_details.html', current_page='place-details')
 
 @home_bp.route('/login')
 def home_login():
-    return render_template('login.html')
+    return render_template('login.html', current_page='login')
 
-@home_bp.route('/add_review')
-def home_add_review():
-    return render_template('add_review.html')
+# @home_bp.route('/add_review')
+# def home_add_review():
+#     return render_template('add_review.html', current_page='add-review')
+
+@home_bp.route('/register')
+def home_register_new_user():
+    return render_template('register_new_user.html', current_page='register')
